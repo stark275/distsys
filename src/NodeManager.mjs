05aee352f866d874b91647c64534f8a0d6ae8588
MUMBERE MALULE Jacques <stark1_5@live.com>
2021-08-30 21:56:06 +0200

    Fix asynch Request in pingNode()

--- src/NodeManager.mjs
+++ src/NodeManager.mjs
@@ -3,43 +3,43 @@ class NodeManager {
     constructor(httpModule,nodes) {
         this.nodes = nodes
         this.httpModule = httpModule
+        this.aliveMessage = 'alive'
+        this.currentNode = -1;
+    }
+
+  
+    pingNodes(i){                                 
+        var request= this.httpModule.request(this.nodes[i].options, this.requestCallback);
+        request.on('error', (err) => {
+                
+            //  console.log(this.nodes[this.currentNode]);     
+            //  console.log(this.nodes[i].options)
+            console.log(this.nodes[i])              
+            console.error('Error with the request:', err.message)        
+        });
+        request.end();
+        
+        if (this.currentNode == this.nodes.length)  
+            this.currentNode = -1
+
+        this.currentNode++
+            console.error('-----------------------------------------------------')              
+
     }
 
     requestCallback(response){
-        var str = '';
+        var str = ''
         response.on('data', function (chunk) {
-            str += chunk;
+            str += chunk
         });
 
-        //the whole response has been recieved, so we just print it out here
         response.on('end', function () {
-            console.log(str);
+            console.log(str)
             //console.log('From: '+options.protocol+'//'+options.host+':'+options.port+options.path);
         });
     }
 
-    pingNodes(){
-        setInterval(() => {
-            this.nodes.forEach(options => {
-            
-            // var options = {
-            //     protocol:'http:',  
-            //     host: '127.0.0.1',
-            //     port:5000,
-            //     path: '/',
-            //     method:'GET'
-            // };
-
-            var request= this.httpModule.request(options, this.requestCallback);
-
-            request.on('error', function(err) {
-                    // handle errors with the request itself
-                    console.error('Error with the request:', err.message);        
-            });
-
-            request.end();
-            });
-        }, 3000);
+    getAliveNode(){
 
     }
 }
--- test.mjs
+++ test.mjs
@@ -1,16 +1,53 @@
 import pkg from './src/NodeManager.mjs';
-const { NodeManager } = pkg;
 import * as http from 'http';
+
+const { NodeManager } = pkg;
+
 
 let nodes = [
     {
-        protocol:'http:',  
-        host: '127.0.0.1',
-        port:5000,
-        path: '/',
-        method:'GET'
-    }
+       'options' :{
+            protocol:'http:',  
+            host: '127.0.0.1',
+            port:5000,
+            path: '/',
+            method:'GET'
+        },
+        'type': 'server',
+        'state': 1,
+    },
+    {
+       'options' :{
+            protocol:'http:',  
+            host: '127.0.0.1',
+            port:6000,
+            path: '/test',
+            method:'GET'
+        },
+        'type': 'server',
+        'state': 0,
+    },
+    {
+       'options' :{
+            protocol:'http:',  
+            host: '127.0.0.1',
+            port:4000,
+            path: '/test',
+            method:'GET'
+        },
+        'type': 'server',
+        'state': 2,
+    },
 ]
 
 let nm = new NodeManager(http,nodes)
-nm.pingNodes()
+var i = 0
+ 
+setInterval(() => {
+    nm.pingNodes(i)
+    console.log(i); 
+    if (i == nodes.length - 1) 
+        i = 0
+    else
+        i++
+}, 1000);
